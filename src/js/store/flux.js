aa0f58993a84a8f557d2c85640898fd1b0451fd7
Jorge <110149668+JorgeMRs@users.noreply.github.com>
2023-08-16 21:05:48 +0000

    Falta

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,24 @@
-const getState = ({ getStore, setStore }) => {
+const getState = ({ getStore, setStore, getAction }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contacts: []
 		},
+
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+
+			obtenerInfo: async function() {
+				try {
+					let response = await fetch("https://playground.4geeks.com/apis/fake/contact/agenda/juana");
+					let data = await response.json();
+					console.log(data);
+					setStore({ contacts: data });
+				} catch (error) {
+					console.log(error);
+				}
+			}
 		}
 	};
 };
